44721fc140c53bd298c61f0b31d5773d24395566
Josua Blejeru <josua@blejeru.com>
2023-08-11 00:12:51 +0200

    :technologist: setup the database creation with some data

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
     id("org.graalvm.buildtools.native") version "0.9.23"
+    id("org.liquibase.gradle") version "2.0.4"
 }
 
 group = "com.josuablejeru"
@@ -16,6 +17,10 @@ configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
     }
+    liquibase {
+        activities.register("main")
+        runList = "main"
+    }
 }
 
 repositories {
@@ -24,12 +29,19 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.liquibase:liquibase-core")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:junit-jupiter:1.18.3")
+
+    liquibaseRuntime("org.postgresql:postgresql")
+    liquibaseRuntime("org.liquibase.ext:liquibase-hibernate5:4.3.5")
 }
 
 tasks.withType<Test> {
---
+++ src/main/resources/db/changelog/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO product (name)
+VALUES ('Coca-Cola Zero'),
+       ('Star Pufuleti'),
+       ('Borsec');
