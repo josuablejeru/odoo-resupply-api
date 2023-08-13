c479b14950e2c7c62579568f7b183962be65d369
Josua Blejeru <josua@blejeru.com>
2023-08-13 21:24:46 +0200

    :memo: write a description for the OpenAPI spec

--- src/main/java/com/josuablejeru/resupplyapi/ResupplyApiApplication.java
+++ src/main/java/com/josuablejeru/resupplyapi/ResupplyApiApplication.java
@@ -1,11 +1,25 @@
 package com.josuablejeru.resupplyapi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Resupply API",
+                description = """
+                        Scope of the API is to serve a Mobile Application for users of Odoo
+                        If someone needs a resupply of certain Products this application will handle the request
+                        """,
+                contact = @Contact(
+                        name = "Josua Blejeru",
+                        email = "josua@blejeru.com"
+                )
+        )
+)
 public class ResupplyApiApplication {
 
     public static void main(String[] args) {
