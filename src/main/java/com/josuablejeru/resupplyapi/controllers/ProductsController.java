a8b46e4f500b3d2c98ae136815c68634ef587fa1
Josua Blejeru <josua@blejeru.com>
2023-08-13 21:23:35 +0200

    :loud_sound: add logs then endpoints are hit

--- src/main/java/com/josuablejeru/resupplyapi/controllers/ProductsController.java
+++ src/main/java/com/josuablejeru/resupplyapi/controllers/ProductsController.java
@@ -2,6 +2,8 @@ package com.josuablejeru.resupplyapi.controllers;
 
 import com.josuablejeru.resupplyapi.models.Product;
 import com.josuablejeru.resupplyapi.services.ProductService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,6 +14,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/products")
+@Tag(name = "Products Resupply", description = "requests products you need in your inventory")
+@Log4j2
 public class ProductsController {
 
     private final ProductService productService;
@@ -21,14 +25,17 @@ public class ProductsController {
         this.productService = productService;
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> getAllProducts() {
+        log.info("Request all Products");
         return this.productService.list();
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Product> requestProduct(@RequestBody Product product) {
         Product createdProduct = this.productService.add(product);
+
+        log.debug("Create new Product: {}", product);
 
         return new ResponseEntity<>(createdProduct, HttpStatus.OK);
     }
