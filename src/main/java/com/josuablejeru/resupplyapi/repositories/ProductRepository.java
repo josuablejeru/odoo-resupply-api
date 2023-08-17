84d9808c19e17c3201642dff8b087294be81e00e
Josua Blejeru <josua@blejeru.com>
2023-08-17 21:20:47 +0200

    :bug: as the Product JPA uses a String type the repository should do the same

--- src/main/java/com/josuablejeru/resupplyapi/repositories/ProductRepository.java
+++ src/main/java/com/josuablejeru/resupplyapi/repositories/ProductRepository.java
@@ -4,9 +4,8 @@ import com.josuablejeru.resupplyapi.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 
 }
